28f1885f1e00596f2e652c4f8656341245a5ebbb
murshidmhd <murshidmuhammad65@gmail.com>
2026-05-07 09:52:23 +0530

    feat: enable django admin and register tenant models

--- django/backend/urls.py
+++ django/backend/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -24,6 +25,7 @@ from drf_spectacular.views import (
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/docs/",
